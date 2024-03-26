75269e1ba85fd8311b3536e1fa8c6ea5576dbb3a
k632z823 <k632z823@wichita.edu>
2024-03-25 22:39:38 -0500

    added tailwind framework

--- prj-app/src/App.tsx
+++ prj-app/src/App.tsx
@@ -1,27 +1,8 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
+import Nav from "./lib/Nav";
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Welcome to MS-TIPS
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+	return <Nav></Nav>;
 };
 
 export default App;
