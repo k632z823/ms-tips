19c5b5c5f8db32d92742cb61789979c4f618f06b
k632z823 <k632z823@wichita.edu>
2024-03-28 00:24:27 -0500

    removed unneccesary imports

--- prj-app/src/App.tsx
+++ prj-app/src/App.tsx
@@ -2,11 +2,6 @@ import type { Component } from "solid-js";
 
 import Nav from "./lib/Nav";
 
-
-import Overview from "./pages/Overview";
-import Entries from "./pages/Entries";
-import Archive from "./pages/Archive";
-
 const App: Component = (props: any) => {
 	return (
 		<>
