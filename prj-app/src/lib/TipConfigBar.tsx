26497b47660cd83325e8dce0ed5524eaf3c64900
k632z823 <k632z823@wichita.edu>
2024-04-06 01:10:30 -0500

    Add Tip Config summary bar

--- prj-app/src/lib/TipConfigBar.tsx
+++ prj-app/src/lib/TipConfigBar.tsx
@@ -8,8 +8,33 @@ interface TipConfigProps {
 const TipConfigBar: Component<TipConfigProps> = (props: TipConfigProps) => {
 	return (
 		<>
-			<div>tip total = {props.tip_total}</div>
-			<div>tip rate = {props.tip_rate}</div>
+			<div
+				class='flex justify-center px-5'
+				id='tip-config-bar'
+			>
+				<div class='border border-border-gray rounded-md w-full'>
+					<table class='table-auto w-full'>
+						<tbody>
+							<tr class='border-border-gray'>
+								<td class='p-2 align-top border-r border-border-gray text-xs text-mini-gray'>
+									Total Tip Amount
+								</td>
+								<td class='p-2 align-top border-r border-border-gray text-xs text-mini-gray'>
+									Tip Rate
+								</td>
+							</tr>
+							<tr class='border-border-gray'>
+								<td class='px-2 pb-2 w-1/3 border-r border-border-gray text-l text-right'>
+									${props.tip_total}
+								</td>
+								<td class='px-2 pb-2 w-1/3 border-r border-border-gray text-l text-right'>
+									${props.tip_rate}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</>
 	);
 };
