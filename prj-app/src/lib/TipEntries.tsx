5fc904725fa8960406982ae1ab1aabdfa2f118a0
zfmun <zfmun2000@gmail.com>
2024-04-11 20:58:37 -0500

    minor classification dropdown fix

--- prj-app/src/lib/TipEntries.tsx
+++ prj-app/src/lib/TipEntries.tsx
@@ -234,8 +234,8 @@ const EntryDisplay: Component = () => {
 															stroke-width="0"
 															xmlns="http://www.w3.org/2000/svg"
 															viewBox="0 0 16 16"
-															height="1em"
-															width="1em"
+															height="1.2em"
+															width="1.2em"
 															style="overflow: visible; color: currentcolor;"
 														>
 															<path fill="currentColor" d="m15.89 10.188-4-5A.5.5 0 0 0 11.5 5h-7a.497.497 0 0 0-.39.188l-4 5A.5.5 0 0 0 0 10.5V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4.5a.497.497 0 0 0-.11-.312zM15 11h-3.5l-2 2h-3l-2-2H1v-.325L4.74 6h6.519l3.74 4.675V11z"></path>
@@ -260,8 +260,8 @@ const EntryDisplay: Component = () => {
 															stroke-width="0"
 															xmlns="http://www.w3.org/2000/svg"
 															viewBox="0 0 640 512"
-															height="1em" 
-															width="1em" 
+															height="1.2em" 
+															width="1.2em" 
 															style="overflow: visible; color: currentcolor;"
 														>
 															<path d="M96 96v224c0 35.3 28.7 64 64 64h416c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H160c-35.3 0-64 28.7-64 64zm64 160c35.3 0 64 28.7 64 64h-64v-64zm64-160c0 35.3-28.7 64-64 64V96h64zm352 160v64h-64c0-35.3 28.7-64 64-64zM512 96h64v64c-35.3 0-64-28.7-64-64zM288 208a80 80 0 1 1 160 0 80 80 0 1 1-160 0zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120v240c0 66.3 53.7 120 120 120h400c13.3 0 24-10.7 24-24s-10.7-24-24-24H120c-39.8 0-72-32.2-72-72V120z"></path>
