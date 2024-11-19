e7f41c6b3f789b454bc89d043c7892470cbd3736
Nam Nguyen <namnguyen4575@gmail.com>
2024-11-18 22:26:15 -0600

    adjust edit to route to correct archive_entry + entry_no

--- prj-app/src/lib/ArchiveTable.tsx
+++ prj-app/src/lib/ArchiveTable.tsx
@@ -399,7 +399,8 @@ const ArchiveTable: Component = () => {
 																							].momentDate.format(
 																								"MM-DD-YYYY",
 																							) +
-																							"/0",
+																							"/0" +
+																							entryRows[selectedEntry()].entry.entry_no,
 																						{
 																							replace: true,
 																						},
@@ -612,7 +613,7 @@ const ArchiveTable: Component = () => {
 				</div>
 			</Show>
 			<div>
-				{/* view entry window */}
+				{/* VIEW ENTRY WINDOW, APEPARS AFTER CLICKING VIEW */}
 				<Show when={rendered()}>
 					<Show when={sortedEntryRows[selectedEntry()].viewShown}>
 						<Portal>
@@ -723,6 +724,7 @@ const ArchiveTable: Component = () => {
 											</button>
 										</div>
 										<div class='justify-self-end'>
+											{/* PENCIL - EDIT BUTTON */}
 											<button
 												class='mr-1 p-2 rounded-md hover:bg-border-gray'
 												onClick={() => {
@@ -731,7 +733,8 @@ const ArchiveTable: Component = () => {
 															entryRows[selectedEntry()].momentDate.format(
 																"MM-DD-YYYY",
 															) +
-															"/0",
+															"/0" +
+															entryRows[selectedEntry()].entry.entry_no,
 														{
 															replace: true,
 														},
@@ -750,6 +753,7 @@ const ArchiveTable: Component = () => {
 													<path d='M257.7 752c2 0 4-.2 6-.5L431.9 722c2-.4 3.9-1.3 5.3-2.8l423.9-423.9a9.96 9.96 0 0 0 0-14.1L694.9 114.9c-1.9-1.9-4.4-2.9-7.1-2.9s-5.2 1-7.1 2.9L256.8 538.8c-1.5 1.5-2.4 3.3-2.8 5.3l-29.5 168.2a33.5 33.5 0 0 0 9.4 29.8c6.6 6.4 14.9 9.9 23.8 9.9zm67.4-174.4L687.8 215l73.3 73.3-362.7 362.6-88.9 15.7 15.6-89zM880 836H144c-17.7 0-32 14.3-32 32v36c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-36c0-17.7-14.3-32-32-32z'></path>
 												</svg>
 											</button>
+											{/* TRASH CAN - DELETE BUTTON */}
 											<button
 												class='p-2 rounded-md hover:bg-select-red'
 												onclick={() => {
